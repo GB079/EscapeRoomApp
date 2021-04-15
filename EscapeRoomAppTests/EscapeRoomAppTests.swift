05e3c2987f35eabe98f5a22af5711f6576c2bd0c
xgtes <xdfg>
2021-04-15 12:55:53 -0400

    Initial Commit

---
+++ EscapeRoomAppTests/EscapeRoomAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  EscapeRoomAppTests.swift
+//  EscapeRoomAppTests
+//
+//  Created by Gage Beatty on 4/15/21.
+//  Copyright © 2021 Gage Beatty. All rights reserved.
+//
+
+import XCTest
+@testable import EscapeRoomApp
+
+class EscapeRoomAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
