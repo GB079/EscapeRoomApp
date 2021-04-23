20d13135d7a041ba1cdc80b1149606f06fea8911
xgtes <xdfg>
2021-04-23 13:50:01 -0400

    More Escape Room Progress

---
+++ EscapeRoomApp/QuestionAnswer.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+
+struct QuestionAnswer {
+    // type of question.
+    var topic: String
+    // The question itself.
+    var Question: String
+    // Answer and detraction responses.
+    var answer: String
+    var distractOne: String
+    var distractTwo: String
+    var distractThree: String
+    
+}
+
--- EscapeRoomApp/ViewController.swift
+++ EscapeRoomApp/ViewController.swift
@@ -3,26 +3,26 @@ import UIKit
 class ViewController: UIViewController {
     
 //    @IBOutlet weak var questionLabel: UILabel!
-    
-    @IBOutlet weak var questionView: UITextView!
+//    @IBOutlet weak var questionView: UITextView!
+    var qOne = QuestionAnswer
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func answerOne(_ sender: UIButton) {
-        questionView.text = "Wrong"
-    }
-    @IBAction func answerTwo(_ sender: UIButton) {
-        questionView.text = "Wrong"
-    }
-    @IBAction func answerThree(_ sender: UIButton) {
-        questionView.text = "Correct!"
-    }
-    @IBAction func answerFour(_ sender: UIButton) {
-        questionView.text = "Wrong"
-    }
+//    @IBAction func answerOne(_ sender: UIButton) {
+//        questionView.text = "Wrong"
+//    }
+//    @IBAction func answerTwo(_ sender: UIButton) {
+//        questionView.text = "Wrong"
+//    }
+//    @IBAction func answerThree(_ sender: UIButton) {
+//        questionView.text = "Correct!"
+//    }
+//    @IBAction func answerFour(_ sender: UIButton) {
+//        questionView.text = "Wrong"
+//    }
     
 }
 
