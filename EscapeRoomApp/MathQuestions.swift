294fb0efa8190b41202fe8b2fcdb08749eea86a5
xgtes <xdfg>
2021-05-03 13:25:15 -0400

    Escape Room Progress

---
+++ EscapeRoomApp/MathQuestions.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct MathQuestions {
+    var Questions: [String] = []
+    var answers: [String] = []
+    var distractionsOne: [String] = []
+    var distractionsTwo: [String] = []
+}
--- EscapeRoomApp/ViewController.swift
+++ EscapeRoomApp/ViewController.swift
@@ -2,12 +2,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    var qOne = QuestionAnswer
+    var qOne = QuestionAnswer(topic: "Math", Question: "4x + 16 = 60. Solve for X", answer: "x = 11", distractOne: "x = 8", distractTwo: "x = 15", distractThree: "x = 4")
+    @IBOutlet weak var topicLabel: UILabel!
+    @IBOutlet weak var qLabel: UILabel!
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var distractOneButton: UIButton!
+    @IBOutlet weak var distractTwoButton: UIButton!
+    @IBOutlet weak var distractThreeButton: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        topicLabel.text = qOne.topic
+        qLabel.text = qOne.Question
+        aButton.setTitle("\(qOne.answer)", for: .normal)
+        distractOneButton.setTitle("\(qOne.distractOne)", for: .normal)
+        distractTwoButton.setTitle("\(qOne.distractTwo)", for: .normal)
+        distractThreeButton.setTitle("\(qOne.distractThree)", for: .normal)
     }
+    
     
 }
 
